55e674be2dda81ba38d5f7a8334515c2359608cf
Fr33maan <coo@kapital.games>
2021-01-26 18:49:44 +0700

    FIX - remove Qonversion from bundle

--- android/app/src/main/java/com/boucheaoreilles/MainApplication.java
+++ android/app/src/main/java/com/boucheaoreilles/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.QonversionPackage;
 import ca.jaysoo.extradimensions.ExtraDimensionsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactInstanceManager;
